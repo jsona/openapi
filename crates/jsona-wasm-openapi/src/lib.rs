ce6fa00f70a3ec57a49b2bdfbc97f8679774cc18
sigoden <sigoden@gmail.com>
2022-09-06 14:38:46 +0800

    refactor: wasm js module

--- crates/jsona-wasm-openapi/src/lib.rs
+++ crates/jsona-wasm-openapi/src/lib.rs
@@ -1,19 +1,40 @@
-use jsona::{dom::Node, util::mapper::Mapper};
+use jsona::{dom::Node, error::ErrorObject, util::mapper::Mapper};
 use jsona_openapi::Openapi;
 use std::str::FromStr;
 use wasm_bindgen::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct ParseResult {
+    value: Option<Openapi>,
+    errors: Option<Vec<ErrorObject>>,
+}
 
 #[wasm_bindgen]
-pub fn parse(input: &str) -> Result<JsValue, JsValue> {
+pub fn parse(input: &str) -> JsValue {
     let mapper = Mapper::new_utf16(input, false);
-    let node = Node::from_str(input)
-        .map_err(|err| JsValue::from_serde(&err.to_error_objects(&mapper)).unwrap())?;
-    let openapi = Openapi::try_from(&node).map_err(|errs| {
-        let error_objects: Vec<_> = errs
-            .iter()
-            .map(|v| v.to_error_object(&node, &mapper))
-            .collect::<Vec<_>>();
-        JsValue::from_serde(&error_objects).unwrap()
-    })?;
-    Ok(JsValue::from_serde(&openapi).unwrap())
+    let node = match Node::from_str(input) {
+        Ok(v) => v,
+        Err(err) => {
+            return JsValue::from_serde(&ParseResult {
+                value: None,
+                errors: Some(err.to_error_objects(&mapper)),
+            }).unwrap()
+        }
+    };
+    let result = match Openapi::try_from(&node) {
+        Ok(v) => ParseResult {
+            value: Some(v),
+            errors: None,
+        },
+        Err(errs) => ParseResult {
+            value: None,
+            errors: Some(
+                errs.iter()
+                    .map(|v| v.to_error_object(&node, &mapper))
+                    .collect::<Vec<_>>(),
+            ),
+        },
+    };
+	JsValue::from_serde(&result).unwrap()
 }
---
+++ js/openapi/index_web.d.ts
@@ -0,0 +1,4 @@
+export { OpenapiTypes } from "./index";
+import type * as Module from "./index";
+
+export default function init(input: URL | RequestInfo): Promise<Omit<typeof Module, "OpenapiTypes">>;
